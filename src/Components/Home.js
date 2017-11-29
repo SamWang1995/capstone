9bc1915398c025f3baada8609e3d50b9a480c2a2
JakeWiens <jacobzzss@gmail.com>
2017-11-28 21:04:06 -0700

    Add files via upload

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import HomeHeader from './HomeHeader.js';
 //import * as Colors from 'material-ui/styles/colors';
 
 class Home extends Component {
     render() {
         return (
-            <div>Home</div>
+            <div><HomeHeader /></div>
         );
     }
 }
